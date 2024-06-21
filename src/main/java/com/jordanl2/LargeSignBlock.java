9a7cb42f7eec5a627b6bdceba2c8c8f01012b984
Jordan Leppert <jordanleppert@gmail.com>
2024-06-21 13:52:30 +0100

    Sign is now waterloggable

--- src/main/java/com/jordanl2/LargeSignBlock.java
+++ src/main/java/com/jordanl2/LargeSignBlock.java
@@ -6,16 +6,19 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
 import net.minecraft.data.client.TextureKey;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
@@ -29,10 +32,12 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 
-public class LargeSignBlock extends HorizontalFacingBlock {
+public class LargeSignBlock extends HorizontalFacingBlock implements Waterloggable {
 	
 	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 	public static final EnumProperty<LargeSignCharacter> CHAR = EnumProperty.of("char", LargeSignCharacter.class);
 	
 	public static final Identifier ID = new Identifier("jordanl2", "large_sign");
@@ -49,14 +54,18 @@ public class LargeSignBlock extends HorizontalFacingBlock {
 	
 	public LargeSignBlock(Settings settings) {
         super(settings);
-        setDefaultState(getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
-        setDefaultState(getDefaultState().with(CHAR, LargeSignCharacter.SPACE));
+        setDefaultState(getDefaultState()
+        		.with(Properties.HORIZONTAL_FACING, Direction.NORTH)
+        		.with(WATERLOGGED, false)
+        		.with(CHAR, LargeSignCharacter.SPACE));
 	}
 	
 	@Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(FACING);
-		builder.add(CHAR);
+		builder.add(
+				FACING,
+				WATERLOGGED,
+				CHAR);
     }
 	
 	@Override
@@ -89,7 +98,22 @@ public class LargeSignBlock extends HorizontalFacingBlock {
 	
     @Override
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
-		return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing());
+		return super.getPlacementState(ctx)
+				.with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing())
+				.with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER));
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
     }
     
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+ 
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
 }
