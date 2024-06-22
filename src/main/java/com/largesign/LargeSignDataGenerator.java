4135ddc126d4902e522df73e58ad48c501c28c0d
Jordan Leppert <jordanleppert@gmail.com>
2024-06-22 12:01:50 +0100

    Datagen - made a loop for directions

--- src/main/java/com/largesign/LargeSignDataGenerator.java
+++ src/main/java/com/largesign/LargeSignDataGenerator.java
@@ -1,5 +1,9 @@
 package com.largesign;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
@@ -13,6 +17,7 @@ import net.minecraft.data.client.Model;
 import net.minecraft.data.client.MultipartBlockStateSupplier;
 import net.minecraft.data.client.TextureMap;
 import net.minecraft.data.client.VariantSettings;
+import net.minecraft.data.client.VariantSettings.Rotation;
 import net.minecraft.data.client.When;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
@@ -29,55 +34,27 @@ public class LargeSignDataGenerator implements DataGeneratorEntrypoint {
 		public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
 			blockStateModelGenerator.excludeFromSimpleItemModelGeneration(LargeSignBlock.LARGE_SIGN_BLOCK);
 			MultipartBlockStateSupplier multiPartBlockStateSupplier = MultipartBlockStateSupplier.create(LargeSignBlock.LARGE_SIGN_BLOCK);
+
+			List<Map.Entry<Direction, VariantSettings.Rotation>> directions = new ArrayList<>();
+			directions.add(new AbstractMap.SimpleEntry<Direction, Rotation>(Direction.NORTH, VariantSettings.Rotation.R0));
+			directions.add(new AbstractMap.SimpleEntry<Direction, Rotation>(Direction.EAST, VariantSettings.Rotation.R90));
+			directions.add(new AbstractMap.SimpleEntry<Direction, Rotation>(Direction.SOUTH, VariantSettings.Rotation.R180));
+			directions.add(new AbstractMap.SimpleEntry<Direction, Rotation>(Direction.WEST, VariantSettings.Rotation.R270));
 			
 			for (LargeSignCharacter character : LargeSignCharacter.values()) {
-				multiPartBlockStateSupplier
-					.with(When.allOf(
-							When.create().set(LargeSignBlock.FACING, Direction.NORTH),
-							When.create().set(LargeSignBlock.CHAR, character)
-						  ),
-						  BlockStateVariant.create()
-						  	.put(VariantSettings.Y, 
-						  		 VariantSettings.Rotation.R0)
-						  	.put(VariantSettings.MODEL, 
-								 character.getPath())
-						  	);
-
-				multiPartBlockStateSupplier
+				for (Map.Entry<Direction, VariantSettings.Rotation> direction : directions) {
+					multiPartBlockStateSupplier
 						.with(When.allOf(
-								When.create().set(LargeSignBlock.FACING, Direction.EAST),
+								When.create().set(LargeSignBlock.FACING, direction.getKey()),
 								When.create().set(LargeSignBlock.CHAR, character)
 							  ),
 							  BlockStateVariant.create()
 							  	.put(VariantSettings.Y, 
-							  		 VariantSettings.Rotation.R90)
+							  		 direction.getValue())
 							  	.put(VariantSettings.MODEL, 
 									 character.getPath())
-							  	);
-
-				multiPartBlockStateSupplier
-						.with(When.allOf(
-								When.create().set(LargeSignBlock.FACING, Direction.SOUTH),
-								When.create().set(LargeSignBlock.CHAR, character)
-							  ),
-							  BlockStateVariant.create()
-							  	.put(VariantSettings.Y, 
-							  		 VariantSettings.Rotation.R180)
-							  	.put(VariantSettings.MODEL, 
-									 character.getPath())
-							  	);
-
-				multiPartBlockStateSupplier
-						.with(When.allOf(
-								When.create().set(LargeSignBlock.FACING, Direction.WEST),
-								When.create().set(LargeSignBlock.CHAR, character)
-							  ),
-							  BlockStateVariant.create()
-							  	.put(VariantSettings.Y, 
-							  		 VariantSettings.Rotation.R270)
-							  	.put(VariantSettings.MODEL, 
-									 character.getPath())
-							  	);
+						);
+				}
 
 				Model model = new Model(
 						Optional.of(new Identifier(LargeSign.MOD_ID, "block/large_sign_parent")),
