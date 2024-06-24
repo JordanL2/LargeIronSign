0c502fed148a8e9dbbf5451e6371e723cca4e589
Jordan Leppert <jordanleppert@gmail.com>
2024-06-24 14:03:43 +0100

    Reuse single instance of LargeSignModel

--- src/client/java/com/largesign/LargeSignModelLoadingPlugin.java
+++ src/client/java/com/largesign/LargeSignModelLoadingPlugin.java
@@ -7,12 +7,14 @@ import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 @Environment(EnvType.CLIENT)
 public class LargeSignModelLoadingPlugin implements ModelLoadingPlugin {
 	
+	public static final LargeSignModel LARGE_SIGN_MODEL = new LargeSignModel();
+	
     @Override
     public void onInitializeModelLoader(Context pluginContext) {
         pluginContext.modifyModelOnLoad().register((original, context) -> {
             if (context.id().getNamespace().equals(LargeSignBlock.ID.getNamespace())
             		&& context.id().getPath().equals(LargeSignBlock.ID.getPath())) {
-                return new LargeSignModel();
+                return LARGE_SIGN_MODEL;
             } else {
             	return original;
             }
